fb819c72da1483fc6da06477dc7e013eebeaa7aa
brunalopes1996 <bruuna.oliveiraa1@gmail.com>
2025-04-25 20:34:02 -0300

    Criando a interface gráfica

---
+++ todo/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+export class TodoListComponent {
+
+  onSubmit(form: NgForm) {
+    
+  }
+}
